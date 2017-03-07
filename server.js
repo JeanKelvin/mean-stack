50f8226bc775de7df586acd4b6d14b5668171484
jean cardoso <jean.cardoso@b2wdigital.com>
2017-03-07 16:02:46 -0300

    mean

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require('http');
-var app = require('./config/express')
+var app = require('./config/express');
+require('./config/database')('mongodb://localhost/alurapic');
 
 http.createServer(app)
 .listen(3000, function() {
